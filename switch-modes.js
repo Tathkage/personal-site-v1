0bc05483dc881358c8e76be363ef2d02926c3b70
Tathkage <tathluach.krc@gmail.com>
2023-02-06 03:57:45 -0600

    Progress on top navigation bar and title.

--- switch-modes.js
+++ switch-modes.js
@@ -7,26 +7,19 @@ function toggleStyleSheet() {
     if (fileName == "free.css") {
         newFileName = "strict.css";
     }
-
     else {
         newFileName = "free.css";
     }
 
     styleElement.setAttribute("href", newFileName);
-
-    // TASK 2
-    // 2 (d) For persistence when page is refreshed. save new stylesheet name to localStorage
-    // hint: localStorage.setItem(name, value)
     localStorage.setItem("stylesheet", newFileName);
+    
 }
 
 window.onload = function() {
-    // 2 (a) get stylesheet name from local storage hint: localStorage.getItem(name)
+
     const styleSheetName = localStorage.getItem("stylesheet");
-
-    // 2 (b) get html style element by ID
     const htmlStyleElement = document.getElementById("freeStyleSheet");
-
-    // 2 (c) replace href attribute of html element.
     htmlStyleElement.setAttribute("href", styleSheetName);
+
 }
